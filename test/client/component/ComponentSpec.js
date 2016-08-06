96c263417da28820590ed9c2918ed455bcf29088
Andrew Vye <a.vye26@gmail.com>
2016-08-06 16:16:20 -0700

    (feat) Add client testing setup

---
+++ test/client/component/ComponentSpec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+
+describe('Component Test', function () {
+  it('should pass', function () {
+    const testExample = true;
+    expect(testExample).to.equal(true);
+  });
+});
