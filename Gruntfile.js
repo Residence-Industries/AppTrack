ba45e34464730521f4bc0d6db0aecc043416d845
Andrew Vye <a.vye26@gmail.com>
2016-08-08 16:27:32 -0700

    (feat) Add grunt-istanbul to test coverage with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,31 +8,56 @@ module.exports = function(grunt) {
           reporter: 'spec',
           require: 'babel-register'
         },
-        src: ['test/**/*.js']
+        src: ['test/client/**/*.js', 'test/integration/**/*.js', 'test/client/**/*.js']
       }
     },
 
     // code coverage settings
+    env: {
+      coverage: {
+        APP_DIR_FOR_CODE_COVERAGE: './test/coverage/instrument/app/'
+      }
+    },
+
+    instrument: {
+      files: 'client/**/*.js',
+      options: {
+        lazy: true,
+        basePath: 'test/coverage/instrument/'
+      }
+    },
+
+    storeCoverage: {
+      options: {
+        dir: 'test/coverage/reports'
+      }
+    },
+
+    makeReport: {
+      src: 'test/coverage/reports/**/*.json',
+      options: {
+        type: 'lcov',
+        dir: 'test/coverage/reports',
+        print: 'detail'
+      }
+    }
 
   });
 
   grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-istanbul');
-
-  grunt.registerTask('build', [
-    'babel',
-    'concat',
-    'uglify',
-    'sass',
-    'cssmin'
-  ]);
 
   grunt.registerTask('test', [
     'mochaTest'
+  ]);
+
+  grunt.registerTask('coverage', [
+    'env:coverage',
+    'instrument',
+    'mochaTest',
+    'storeCoverage',
+    'makeReport'
   ]);
 }
