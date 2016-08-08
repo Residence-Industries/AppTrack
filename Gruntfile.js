e45586271a8c49809e905802cdb48df12e400edf
Andrew Vye <a.vye26@gmail.com>
2016-08-08 13:28:55 -0700

    (feat) Utilize grunt for running tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,19 @@ module.exports = function(grunt) {
           'build/styles.min.css': 'client/styles/styles.css'
         }
       }
-    }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          require: 'babel-register'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    // code coverage settings
 
   });
 
@@ -45,11 +57,17 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-istanbul');
 
   grunt.registerTask('build', [
     'concat',
     'uglify',
     'sass',
     'cssmin'
+  ]);
+
+  grunt.registerTask('test', [
+    'mochaTest'
   ]);
 }
--- test/integration/ApiSpec.js
+++ test/integration/ApiSpec.js
@@ -3,3 +3,21 @@ import { supertest } from 'supertest';
 
 import app from '../../server/app';
 // var supertest = supertest(app);
+// var port = process.env.PORT || 4000;
+
+// describe('API Test', () => {
+//   var server;
+//   var testItem = {
+//     isAnItem: true
+//   };
+//
+//   before((done) => {
+//     server = app.listen(port, () => {
+//       done();
+//     });
+//   });
+//
+//   after((done) => {
+//     server.close(done);
+//   });
+// })
