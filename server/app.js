b4d020b65966ce1e3abd053b91739b1e5be8f0eb
Keeley Nakamoto <keeleynakamoto@gmail.com>
2016-08-10 14:21:27 -0700

    Setup intial server and database structure

--- server/app.js
+++ server/app.js
@@ -1,17 +1,29 @@
+var express = require('express');
+var cors = require ('cors');
+var bodyParser = require('body-parser');
+var path = require('path');
 var request = require('request');
-var host = 'localhost';
-var port = 7474;
-var urlForPosting = 'http://' + host + ':' + port + '/db/data/transaction/commit';
 
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+//Logger for dev environment
+if (process.env.NODE_ENV !== 'production') {
+  var morgan = require('morgan');
+  app.use(morgan('dev'));
+}
+
+//Middleware
+app.use(cors());
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+//parse application/json
+app.use(bodyParser.json());
+//serve up static files in production
+//app.use('/', express.static(path.resolve(__dirname, '../build')));
+app.use(express.static(__dirname + '/../client'));
+
+//Neo4j setup
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
-
-
-var node = db.createNode({hello: 'world'});
-node.save((err,node) => {
-  if (err) {
-    console.log('Error saving new node to database: ', err);
-  } else {
-    console.log('Node saved to database with id: ', node.id);
-  }
-})
